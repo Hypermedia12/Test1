36d084a2b0ec613f806b8a4add29d21f4acabe9f
mgmverburg <mgmverburg@gmail.com>
2015-02-09 11:31:34 +0100

    The rest of the base files

---
+++ scripts.js
@@ -0,0 +1,20 @@
+		
+
+		
+		function showMessage() {
+			//alert(document.getElementById('text').value.length);
+			document.getElementById('length').innerHTML = document.getElementById('text').value.length;
+			//alert("Hello, World!"); 
+		}
+		function addBorder() {
+			document.getElementById('wasted').style.border="solid 5px red"
+		}
+		function removeBorder() {
+			document.getElementById('wasted').style.border="0px"
+		}
+		
+		function addition(x,y) {
+			var x = +document.getElementById('xval').value;
+			var y = +document.getElementById('yval').value;
+			document.getElementById('result').innerHTML = x+y;
+		}
